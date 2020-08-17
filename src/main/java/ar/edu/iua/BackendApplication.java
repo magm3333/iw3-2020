077d2534686448a43764e141ee94316a5c13e5a4
Mariano García Mattío <magm3333@gmail.com>
2020-08-17 19:10:54 -0300

    Perfiles maven y spring boot para tiempo de ejecución. Aplicacion de inicio con CommandLineRunner

--- src/main/java/ar/edu/iua/BackendApplication.java
+++ src/main/java/ar/edu/iua/BackendApplication.java
@@ -1,13 +1,28 @@
 package ar.edu.iua;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+	
+	@Value("${spring.datasource.url}")
+	private String springDatasourceUrl;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("DataSource URL: {}", springDatasourceUrl);
+		
 	}
 
 }
