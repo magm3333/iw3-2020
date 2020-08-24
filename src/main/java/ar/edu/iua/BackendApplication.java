f40516b1992572a36691beff794ebf52fc288252
Mariano García Mattío <magm3333@gmail.com>
2020-08-24 08:22:13 -0300

    Clase Martes 18/8/2020

--- src/main/java/ar/edu/iua/BackendApplication.java
+++ src/main/java/ar/edu/iua/BackendApplication.java
@@ -7,7 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
+	
+	
 }
+
+/*
+Modelo
+  | -- > Persistencia
+----------------------------
+Negocio
+----------------------------
+Servicios WEB (REST)
+  | --> servicio 1   |
+  | --> servicio 2   | seguridad
+  | --> servicio N   V
+*/
