c76cdc0e24232cb67179a34fae73645fd2bdad95
magm3333 <magm3333@gmail.com>
2021-05-11 17:15:33 -0300

    Fixes varios dummy

--- src/main/java/ar/edu/iua/rest/DummyRestController.java
+++ src/main/java/ar/edu/iua/rest/DummyRestController.java
@@ -1,6 +1,7 @@
 package ar.edu.iua.rest;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -18,19 +19,43 @@ public class DummyRestController {
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 
-	
-
 	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<String>> listDummy() {
-		List<String> lista=new ArrayList<String>();
-		lista.add("V8");
-		lista.add("Malon");
-		lista.add("Hermetica");
-		
+	public ResponseEntity<List<Banda>> listDummy() {
+		List<Banda> lista = new ArrayList<Banda>();
+		lista.add(new Banda("V8", new Date()));
+		lista.add(new Banda("Malon", new Date()));
+		lista.add(new Banda("Hermética", new Date()));
 
-		return new ResponseEntity<List<String>>(lista, HttpStatus.OK);
+		return new ResponseEntity<List<Banda>>(lista, HttpStatus.OK);
 	}
 
+}
 
+class Banda {
+
+	private String nombre;
+	private Date fecha;
+
+	public Banda(String nombre, Date fecha) {
+		super();
+		this.nombre = nombre;
+		this.fecha = fecha;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
 }
